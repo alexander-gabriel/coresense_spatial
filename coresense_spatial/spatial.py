b84eacf5bdfc67906bed81be3e38bc33c7fc23bc
Alexander Gabriel <einalex@4d6.de>
2024-02-22 14:42:15 +0100

    preliminary subscriber and service setup

--- coresense_spatial/spatial.py
+++ coresense_spatial/spatial.py
@@ -1,6 +1,8 @@
 import rclpy
 from rclpy.node import Node
 
+from coresense_msgs.srv import GetObjectRelation
+from coresense_msgs.msg import ObjectUpdate
 from spatial_spec.logic import Spatial
 
 class SpatialService(Node):
@@ -8,20 +10,20 @@ class SpatialService(Node):
     def __init__(self):
         super().__init__('spatial')
         self.spatial = Spatial()
-        #self.subscription = self.create_subscription(
-        #    UpdateMessageClass,
-        #    'update_message_topic',
-        #    self.object_update_callback,
-        #    10)
-        #self.subscription  # prevent unused variable warning
-        #self.srv = self.create_service(ServiceInterfaceClass, 'spatial', self.spatial_callback)
+        self.subscription = self.create_subscription(
+            ObjectUpdate,
+            'update_message_topic',
+            self.object_update_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+        self.srv = self.create_service(GetObjectRelation, 'spatial_relation', self.spatial_callback)
 
     def object_update_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
+        self.get_logger().info('I heard: "%s"' % msg.pose)
 
     def spatial_callback(self, request, response):
-        #response.sum = request.a + request.b
-        #self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
+        response.relation = request.objecta + request.objectb
+        self.get_logger().info('Incoming request\na: %d b: %d' % (request.objecta, request.objectb))
 
         return response
 
